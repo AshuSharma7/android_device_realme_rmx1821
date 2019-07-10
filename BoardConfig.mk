1251dbb49c5a7a4348dbdfcb38bd24e8c22eb673
Ashu Sharma <ashu7073657271@gmail.com>
2019-07-10 19:33:58 +0530

    Fix for Blank Screen

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,7 @@ TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 80
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_SCREEN_BLANK_ON_BOOT := true
+TW_NO_SCREEN_BLANK := true
 
 # Misc
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
